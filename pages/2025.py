f2166271b9b6edf24ca1fb6392d0734d1494cfc4
Rodrigo Kinouchi <rodrigo@kinouchi.com.br>
2025-07-22 17:56:50 -0300

    Pos 4 etapa

--- pages/2025.py
+++ pages/2025.py
@@ -70,7 +70,7 @@ df['Modelo'] = df['Modelo'].map(modelo_map)
 
 # Input do usuário para a última corrida
 ultima_corrida = st.number_input(
-    "Informe o número da última corrida realizada", min_value=1, max_value=24, value=5, step=1)
+    "Informe o número da última corrida realizada", min_value=1, max_value=24, value=7, step=1)
 
 # Substitui "." (etapas futuras) por NaN
 df.iloc[:, 6:ultima_corrida+6] = df.iloc[:, 6:ultima_corrida+6].replace(".", pd.NA)
